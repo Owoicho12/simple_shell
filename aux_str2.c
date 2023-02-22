62917820ea36f9c738c272bc803e39e7c7eff68b
Owoicho12 <115462208+Owoicho12@users.noreply.github.com>
2023-02-22 13:04:06 +0100

    Update aux_str2.c

--- aux_str2.c
+++ aux_str2.c
@@ -1,129 +1,106 @@
 #include "main.h"
 
 /**
- *  * _strdup - duplicates a str in the heap memory.
- *   * @s: Type char pointer str
- *    * Return: duplicated str
- *     */
-char *_strdup(const char *s)
+ * _strcat - concatenate two strings
+ * @dest: char pointer the dest of the copied str
+ * @src: const char pointer the source of str
+ * Return: the dest
+ */
+char *_strcat(char *dest, const char *src)
 {
-		char *new;
-			size_t len;
+	int i;
+	int j;
 
-				len = _strlen(s);
-					new = malloc(sizeof(char) * (len + 1));
-						if (new == NULL)
-									return (NULL);
-							_memcpy(new, s, len + 1);
-								return (new);
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
+
+	dest[i] = '\0';
+	return (dest);
 }
-
 /**
- *  * _strlen - Returns the lenght of a string.
- *   * @s: Type char pointer
- *    * Return: Always 0.
- *     */
-int _strlen(const char *s)
+ * *_strcpy - Copies the string pointed to by src.
+ * @dest: Type char pointer the dest of the copied str
+ * @src: Type char pointer the source of str
+ * Return: the dest.
+ */
+char *_strcpy(char *dest, char *src)
 {
-		int len;
 
-			for (len = 0; s[len] != 0; len++)
-					{
-							}
-				return (len);
+	size_t a;
+
+	for (a = 0; src[a] != '\0'; a++)
+	{
+		dest[a] = src[a];
+	}
+	dest[a] = '\0';
+
+	return (dest);
 }
-
 /**
- *  * cmp_chars - compare chars of strings
- *   * @str: input string.
- *    * @delim: delimiter.
- *     *
- *      * Return: 1 if are equals, 0 if not.
- *       */
-int cmp_chars(char str[], const char *delim)
+ * _strcmp - Function that compares two strings.
+ * @s1: type str compared
+ * @s2: type str compared
+ * Return: Always 0.
+ */
+int _strcmp(char *s1, char *s2)
 {
-		unsigned int i, j, k;
+	int i;
 
-			for (i = 0, k = 0; str[i]; i++)
-					{
-								for (j = 0; delim[j]; j++)
-											{
-															if (str[i] == delim[j])
-																			{
-																								k++;
-																												break;
-																															}
-																	}
-									}
-				if (i == k)
-							return (1);
-					return (0);
+	for (i = 0; s1[i] == s2[i] && s1[i]; i++)
+		;
+
+	if (s1[i] > s2[i])
+		return (1);
+	if (s1[i] < s2[i])
+		return (-1);
+	return (0);
 }
-
 /**
- *  * _strtok - splits a string by some delimiter.
- *   * @str: input string.
- *    * @delim: delimiter.
- *     *
- *      * Return: string splited.
- *       */
-char *_strtok(char str[], const char *delim)
+ * _strchr - locates a character in a string,
+ * @s: string.
+ * @c: character.
+ * Return: the pointer to the first occurrence of the character c.
+ */
+char *_strchr(char *s, char c)
 {
-		static char *splitted, *str_end;
-			char *str_start;
-				unsigned int i, bool;
+	unsigned int i = 0;
 
-					if (str != NULL)
-							{
-										if (cmp_chars(str, delim))
-														return (NULL);
-												splitted = str; /*Store first address*/
-														i = _strlen(str);
-																str_end = &str[i]; /*Store last address*/
-																	}
-						str_start = splitted;
-							if (str_start == str_end) /*Reaching the end*/
-										return (NULL);
-
-								for (bool = 0; *splitted; splitted++)
-										{
-													/*Breaking loop finding the next token*/
-													if (splitted != str_start)
-																	if (*splitted && *(splitted - 1) == '\0')
-																						break;
-															/*Replacing delimiter for null char*/
-															for (i = 0; delim[i]; i++)
-																		{
-																						if (*splitted == delim[i])
-																										{
-																															*splitted = '\0';
-																																			if (splitted == str_start)
-																																									str_start++;
-																																							break;
-																																										}
-																								}
-																	if (bool == 0 && *splitted) /*Str != Delim*/
-																					bool = 1;
-																		}
-									if (bool == 0) /*Str == Delim*/
-												return (NULL);
-										return (str_start);
+	for (; *(s + i) != '\0'; i++)
+		if (*(s + i) == c)
+			return (s + i);
+	if (*(s + i) == c)
+		return (s + i);
+	return ('\0');
 }
-
 /**
- *  * _isdigit - defines if string passed is a number
- *   *
- *    * @s: input string
- *     * Return: 1 if string is a number. 0 in other case.
- *      */
-int _isdigit(const char *s)
+ * _strspn - gets the length of a prefix substring.
+ * @s: initial segment.
+ * @accept: accepted bytes.
+ * Return: the number of accepted bytes.
+ */
+int _strspn(char *s, char *accept)
 {
-		unsigned int i;
+	int i, j, bool;
 
-			for (i = 0; s[i]; i++)
-					{
-								if (s[i] < 48 || s[i] > 57)
-												return (0);
-									}
-				return (1);
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		bool = 1;
+		for (j = 0; *(accept + j) != '\0'; j++)
+		{
+			if (*(s + i) == *(accept + j))
+			{
+				bool = 0;
+				break;
+			}
+		}
+		if (bool == 1)
+			break;
+	}
+	return (i);
 }
